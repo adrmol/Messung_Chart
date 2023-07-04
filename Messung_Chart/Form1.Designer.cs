4a5ef9e85791de1699ed96c6ec990dd4e10c1766
Adrian Molenda <Adrian.Molenda@wibizu.de>
2023-07-04 15:29:51 +0200

    fehlt try catch

--- Messung_Chart/Form1.Designer.cs
+++ Messung_Chart/Form1.Designer.cs
@@ -34,23 +34,29 @@ namespace Messung_Chart
             this.label1 = new System.Windows.Forms.Label();
             this.datum = new System.Windows.Forms.ComboBox();
             this.fuehler_list = new System.Windows.Forms.CheckedListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.connect_button = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart_main)).BeginInit();
             this.SuspendLayout();
             // 
             // chart_main
             // 
+            this.chart_main.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             chartArea1.Name = "ChartArea1";
             this.chart_main.ChartAreas.Add(chartArea1);
-            this.chart_main.Location = new System.Drawing.Point(12, 12);
+            this.chart_main.Location = new System.Drawing.Point(183, 9);
+            this.chart_main.MinimumSize = new System.Drawing.Size(480, 320);
             this.chart_main.Name = "chart_main";
-            this.chart_main.Size = new System.Drawing.Size(776, 426);
+            this.chart_main.Size = new System.Drawing.Size(805, 436);
             this.chart_main.TabIndex = 0;
             this.chart_main.Text = "chart1";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(820, 15);
+            this.label1.Location = new System.Drawing.Point(12, 42);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 1;
@@ -60,7 +66,7 @@ namespace Messung_Chart
             // 
             this.datum.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.datum.FormattingEnabled = true;
-            this.datum.Location = new System.Drawing.Point(867, 12);
+            this.datum.Location = new System.Drawing.Point(56, 39);
             this.datum.Name = "datum";
             this.datum.Size = new System.Drawing.Size(121, 21);
             this.datum.TabIndex = 2;
@@ -68,19 +74,45 @@ namespace Messung_Chart
             // 
             // fuehler_list
             // 
+            this.fuehler_list.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.fuehler_list.CheckOnClick = true;
             this.fuehler_list.FormattingEnabled = true;
-            this.fuehler_list.Location = new System.Drawing.Point(823, 39);
+            this.fuehler_list.Location = new System.Drawing.Point(12, 66);
+            this.fuehler_list.MinimumSize = new System.Drawing.Size(0, 19);
             this.fuehler_list.Name = "fuehler_list";
-            this.fuehler_list.Size = new System.Drawing.Size(165, 394);
+            this.fuehler_list.Size = new System.Drawing.Size(165, 379);
             this.fuehler_list.TabIndex = 3;
             this.fuehler_list.SelectedIndexChanged += new System.EventHandler(this.fuehler_list_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1016, 1);
+            this.panel1.TabIndex = 4;
+            // 
+            // connect_button
+            // 
+            this.connect_button.Location = new System.Drawing.Point(12, 9);
+            this.connect_button.Name = "connect_button";
+            this.connect_button.Size = new System.Drawing.Size(165, 23);
+            this.connect_button.TabIndex = 5;
+            this.connect_button.Text = "Verbinden";
+            this.connect_button.UseVisualStyleBackColor = true;
+            this.connect_button.Click += new System.EventHandler(this.connect_button_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1000, 450);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1000, 456);
+            this.Controls.Add(this.connect_button);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.fuehler_list);
             this.Controls.Add(this.datum);
             this.Controls.Add(this.label1);
@@ -99,6 +131,8 @@ namespace Messung_Chart
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox datum;
         private System.Windows.Forms.CheckedListBox fuehler_list;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button connect_button;
     }
 }
 
--- Messung_Chart/Form1.cs
+++ Messung_Chart/Form1.cs
@@ -2,84 +2,51 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Reflection;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Messung_Chart
 {
-    struct fuehler
-    {
-        public string fName;
-        public List<messwerte> werteListe;
-    }
-    struct messwerte
-    {
-        public DateTime messzeit;
-        public float temperatur;
-    }
-
     public partial class Form1 : Form
     {
         MySqlConnection connection;
-        Series series1;
-        List<fuehler> fuehlerDaten;
         public Form1()
         {
             InitializeComponent();
-            fuehlerDaten = new List<fuehler>();
-
-            string connectionString = "Server=localhost;Database=messwerte;Uid=root;";
-
-            connection = new MySqlConnection(connectionString);
-
-            connection.Open();
-
-            string query = "SELECT DISTINCT Date(datum) AS datum FROM messungen;";
-            MySqlCommand command = new MySqlCommand(query, connection);
-
-            using (MySqlDataReader reader = command.ExecuteReader())
-            {
-                var daten = new List<string>();
-                while (reader.Read())
-                {
-                    datum.Items.Add(reader["datum"].ToString().Substring(0, 10));
-                }
-            }
-
-            chart_main.Legends.Add(new Legend("Legende1"));
-            //series = new Series();
-            //series.ChartType = SeriesChartType.Line;  // Diagrammtyp auf Linie setzen
-            //chart_main.Series.Add(series);
-
+            connect();
         }
 
         private void datum_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string query = "SELECT DISTINCT temperaturfuehler FROM messungen WHERE Date(datum) = @datum;";
-            DateTime.TryParse(datum.SelectedItem.ToString(), out var selected_datum);
-            string mysql_datum = selected_datum.ToString("yyyy-MM-dd");
-            MySqlCommand command = connection.CreateCommand();
-            command.CommandText = query;
-            command.Parameters.AddWithValue("@datum", mysql_datum);
-
-            using (MySqlDataReader reader = command.ExecuteReader())
+            try
             {
-                var daten = new List<string>();
-                while (reader.Read())
-                {
-                    string tf = reader["temperaturfuehler"].ToString();
+                string query = "SELECT DISTINCT temperaturfuehler FROM messungen WHERE Date(datum) = @datum;";
+                DateTime.TryParse(datum.SelectedItem.ToString(), out var selected_datum);
+                string mysql_datum = selected_datum.ToString("yyyy-MM-dd");
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = query;
+                command.Parameters.AddWithValue("@datum", mysql_datum);
 
-                    if (!fuehler_list.Items.Contains(tf))
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    var daten = new List<string>();
+                    while (reader.Read())
                     {
-                        fuehler_list.Items.Add(tf);
+                        string tf = reader["temperaturfuehler"].ToString();
+
+                        if (!fuehler_list.Items.Contains(tf))
+                        {
+                            fuehler_list.Items.Add(tf);
+                        }
                     }
                 }
             }
-
+            catch
+            {
+                MessageBox.Show("Fehler in der Datenbank!");
+                connect();
+            }
             update_chart_main();
-
         }
 
         private void fuehler_list_SelectedIndexChanged(object sender, EventArgs e)
@@ -89,9 +56,9 @@ namespace Messung_Chart
 
         private void update_chart_main()
         {
-            // Get all colors in the System.Drawing.Color namespace
+            // 10 colors in System.Drawing.Color
             Color[] allColors = new System.Drawing.Color[]
-        {
+            {
             System.Drawing.Color.Red,
             System.Drawing.Color.Blue,
             System.Drawing.Color.Green,
@@ -102,73 +69,98 @@ namespace Messung_Chart
             System.Drawing.Color.Black,
             System.Drawing.Color.Olive,
             System.Drawing.Color.Pink
-        };
+            };
 
             int colorNumber = 0;
-
             chart_main.Series.Clear();
-            //series.Points.Clear();
-
-            foreach (var tf_obj in fuehler_list.CheckedItems)
+            try
             {
-                Legend legend = chart_main.Legends["Legend1"];
-                legend.CustomItems.Clear();
-
-                Series series1 = new Series();
-                series1.ChartType = SeriesChartType.Line;  // Diagrammtyp auf Linie setzen
-                chart_main.Series.Add(series1);
-                series1.Color = allColors[colorNumber];
-
-
-                colorNumber++;
-
-                string tf = tf_obj.ToString();
-
-                fuehlerDaten.Add(new fuehler {fName = tf, werteListe = new List<messwerte>() });
-
-                string query = "SELECT temperaturfuehler, datum, temperatur FROM messungen WHERE temperaturfuehler LIKE @tf;";
-                MySqlCommand command = connection.CreateCommand();
-                command.CommandText = query;
-                command.Parameters.AddWithValue("@tf", tf);
-
-                using (MySqlDataReader reader = command.ExecuteReader())
+                foreach (var tf_obj in fuehler_list.CheckedItems)
                 {
-                    for (int i = 0; reader.Read(); i++)
+                    Legend legend = chart_main.Legends["Legend1"];
+                    legend.CustomItems.Clear();
+
+                    Series series1 = new Series();
+                    series1.ChartType = SeriesChartType.Line;  // Diagrammtyp auf Linie setzen
+                    chart_main.Series.Add(series1);
+                    series1.Color = allColors[colorNumber];
+
+
+                    colorNumber++;
+
+                    string tf = tf_obj.ToString();
+                    series1.LegendText = tf;
+
+                    string query = "SELECT temperaturfuehler, datum, temperatur FROM messungen WHERE temperaturfuehler LIKE @tf;";
+                    MySqlCommand command = connection.CreateCommand();
+                    command.CommandText = query;
+                    command.Parameters.AddWithValue("@tf", tf);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
                     {
-                        DateTime.TryParse(reader["datum"].ToString(), out var d);
-                        float.TryParse(reader["temperatur"].ToString(), out var t);
-                        series1.Points.AddXY(d, t);
-
-                        //series1.Points.AddXY(1, 2);  // (x=1, y=2)
-                        //series1.Points.AddXY(2, 4);  // (x=2, y=4)
-                        //series1.Points.AddXY(3, 1);  // (x=3, y=1)
-                        //series1.Points.AddXY(4, 7);  // (x=4, y=7)
-
-                        //fuehlerDaten[i].werteListe.Add(new messwerte {messzeit = d, temperatur = t});
+                        for (int i = 0; reader.Read(); i++)
+                        {
+                            DateTime.TryParse(reader["datum"].ToString(), out var d);
+                            //var uhrzeit = d.TimeOfDay.TotalMilliseconds;
+                            var uhrzeit = d.ToString("HH:mm:ss");
+                            float.TryParse(reader["temperatur"].ToString(), out var t);
+                            series1.Points.AddXY(uhrzeit, t);
+                        }
                     }
                 }
-
-                foreach (Series series in chart_main.Series)
-                {
-                    // Create a legend item with the series name and color
-                    LegendItem legendItem = new LegendItem();
-                    legendItem.Name = tf;
-                    legendItem.Color = series.Color;
-
-                    // Add the legend item to the legend
-                    legend.CustomItems.Add(legendItem);
-                }
             }
-            //foreach(var m in messwerte)
-            //{
-            //    series.Points.AddXY(m.Item2, m.Item3);
-            //}
+            catch
+            {
+                MessageBox.Show("Fehler in der Datenbank!");
+            }
+        }
 
-            // Datenpunkte hinzufügen
-            //series.Points.AddXY(1, 2);  // (x=1, y=2)
-            //series.Points.AddXY(2, 4);  // (x=2, y=4)
-            //series.Points.AddXY(3, 1);  // (x=3, y=1)
-            //series.Points.AddXY(4, 7);  // (x=4, y=7)
+        private void connect_button_Click(object sender, EventArgs e)
+        {
+            connect();
+        }
+
+        private void connect()
+        {
+            try
+            {
+                string connectionString = "Server=localhost;Database=messwerte;Uid=root;";
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+                this.Text = "Verbunden mit Datenbank";
+                try
+                {
+                    string query = "SELECT DISTINCT Date(datum) AS datum FROM messungen;";
+                    MySqlCommand command = new MySqlCommand(query, connection);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        var daten = new List<string>();
+                        while (reader.Read())
+                        {
+                            datum.Items.Add(reader["datum"].ToString().Substring(0, 10));
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Fehler in der Datenbank!");
+                    connect();
+                }
+
+
+
+                chart_main.Legends.Add(new Legend("Legend1"));
+
+                // Set the titles of the X and Y axes
+                chart_main.ChartAreas[0].AxisX.Title = "Uhrzeit";
+                chart_main.ChartAreas[0].AxisY.Title = "Temperatur in °C";
+
+            }
+            catch
+            {
+                MessageBox.Show("Keiner Verbindung zur Datenbank!");
+            }
         }
     }
 }
